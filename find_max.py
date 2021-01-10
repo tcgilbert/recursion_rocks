434206cd2ef243a5cec97287b79969c8d98505b6
Thomas Gilbert <tcgilbert94@gmail.com>
2021-01-10 16:26:08 -0500

    [find_max] done

--- find_max.py
+++ find_max.py
@@ -1,11 +1,18 @@
 # You will have to figure out what parameters to include
-# 🚨 All functions must use recursion 🚨
+# All functions must use recursion
 
 # This function returns the largest number in a given array.
 
 def find_max(l):
-    # Write code here
-    pass
+    if len(l) == 1:
+        return l[0]
+    else:
+        max = find_max(l[1:])
+        if max > l[0]:
+            return max
+        else:
+            return l[0]
 
-# print(find_max([1, 4, 45, 6, -50, 10, 2]))
+
+print(find_max([1, 4, 45, 6, -50, 10, 2]))
 # => 45
